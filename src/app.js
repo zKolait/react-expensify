7bdc3425c9eb9b4f06d391f3a124cea54c73aa3c
Nicolas Chapeau <32775238+zKolait@users.noreply.github.com>
2019-10-23 10:56:03 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Style
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+// Components
+// Main app component here
+
+
+
+// Render
+ReactDOM.render(<p>Hi !</p>, document.getElementById('app'))
+
+
+
